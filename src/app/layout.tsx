9f47c106d30ecf9ab0a387cb80aa3bd60ca30560
sunatchaa <pangsunatcha@gmail.com>
2025-01-01 16:24:44 +0700

    uncommented liff and auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-//import LiffProvider from '@/contexts/LiffContext';
+import LiffProvider from '@/contexts/LiffContext';
 import './globals.css';
 import { IBM_Plex_Sans_Thai } from 'next/font/google';
 import AuthProvider from '@/contexts/AuthContext';
@@ -23,13 +23,13 @@ export default function RootLayout({
             <body
                 className={`${ibmPlexSansThai.className} flex justify-center bg-gray-50`}
             >
-                {/* <LiffProvider> */}
-                    {/* <AuthProvider> */}
+                <LiffProvider>
+                    <AuthProvider>
                         <div className="max-w-screen-sm w-full p-4">
                             {children}
                         </div>
-                    {/* </AuthProvider> */}
-                {/* </LiffProvider> */}
+                    </AuthProvider>
+                </LiffProvider>
             </body>
         </html>
     );
